d60f2ab37211aaea61980195f6f7a7fe180595d1
Nadav Leshem <nadavl@wix.com>
2014-07-16 12:46:31 +0300

    added client side for login/registration

--- controllers/services/index.js
+++ controllers/services/index.js
@@ -1,34 +1,40 @@
-'use strict';
+"use strict";
 
-var rek = require('rekuire');
-var usersDao = rek('beans').db.usersDao;
-var User = rek('User');
-var q = rek('q');
+var rek = require("rekuire");
+var usersDao = rek("beans").db.usersDao;
+var User = rek("User");
+var q = rek("q");
 
 module.exports = function (router) {
 
-    router.post('/login', function (req, res) {
+    router.post("/login", function (req, res) {
         usersDao.getUserByNameAndPassword(req.body.name, req.body.password)
             .then(function (user) {
                 res.send();
             })
             .catch(function (e) {
-                res.status(500).send(e);
+                res.status(500).send("Failed to login. Please check your credentials.");
             });
     });
 
-    router.post('/register', function (req, res) {
-        q.fcall(function(){
+    router.post("/register", function (req, res) {
+        q.fcall(function () {
             return User.create({name: req.body.name, password: req.body.password});
         })
-        .then(function(user){
-            return usersDao.save(user);
-        })
-        .then(function () {
-            res.send();
-        })
-        .catch(function (e) {
-            res.status(500).send(e);
-        });
+            .then(function (user) {
+                return usersDao.save(user);
+            })
+            .then(function () {
+                res.send();
+            })
+            .catch(function (e) {
+                var msg = "";
+                if (e.toString().indexOf("UNIQUE") !== -1) {
+                    msg = "The username you chose is already taken.";
+                } else {
+                    msg = "Failed to register. Please try again later.";
+                }
+                res.status(500).send(msg);
+            });
     });
 };
