2f0cfbe105b88e4fddf510f0667440389bd35e4d
Nadav Leshem <nadavl@wix.com>
2014-07-22 07:57:05 +0300

    writing posts now work! :)

---
+++ test/e2e/RegisterAndLogin.specs.js
@@ -0,0 +1,29 @@
+var rek = require("rekuire");
+var Browser = require("zombie");
+var assert = require("assert");
+
+describe("Register And Login end 2 end", function () {
+    var server, browser, base;
+
+    it("should be able to perform a register", function (done) {
+        browser.visit(base + "/register", function () {
+            browser
+                .fill("username", "zombie@underworld.dead")
+                .fill("password", "eat-the-living")
+                .pressButton("Register", function () {
+                    assert.ok(browser.success);
+                    assert.equal(browser.location.pathname, "/wall");
+                    done();
+                });
+        });
+    });
+
+    beforeEach(function (done) {
+        server = rek("TestServer").create(done);
+        browser = new Browser();
+        base = "http://" + server.address().address + ":" + server.address().port;
+    });
+    afterEach(function () {
+        server.close();
+    })
+});
